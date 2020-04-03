bd8f5efad547a31c1304e8d0814529659c0f9e88
zhuyytt998 <zhuyytt998@163.com>
2020-04-03 16:50:14 +0800

    这是基础SpringBoot项目结构

---
+++ src/main/java/com/lttbookshop/LttbookshopApplication.java
@@ -0,0 +1,13 @@
+package com.lttbookshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LttbookshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LttbookshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lttbookshop/LttbookshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lttbookshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LttbookshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
